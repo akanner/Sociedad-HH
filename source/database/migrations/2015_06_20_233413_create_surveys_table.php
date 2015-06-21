673da10d14d69a892041ea94b6234f0c11b66b83
Agustin Ignacio Kanner <agustinkanner@gmail.com>
2015-06-20 21:18:36 -0300

    Agregue el la clase Survey

---
+++ source/database/migrations/2015_06_20_233413_create_surveys_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('substract');
+            $table->string('email');
+            $table->date('activeFrom');
+            $table->date('activeTo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('surveys');
+    }
+}
