22bd47b1422ca3db4cebd2c59ed147226d1bf002
leito9008 <leito.vm3@hotmail.com>
2015-06-21 19:49:03 -0300

    Cambiado database.php

--- source/resources/views/welcome.blade.php
+++ source/resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Sociedad de Hemat&oacute;logos y Hemoterapeutas de La Plata</title>
 
         <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
