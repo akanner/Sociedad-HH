e4002dc315c6e972b0a9f633999abb602c44538a
leito9008 <leito.vm3@hotmail.com>
2015-06-23 22:24:25 -0300

    Layout básico, incluye assets y templates con herencia

--- source/app/Http/routes.php
+++ source/app/Http/routes.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* Páginas estáticas */
+Route::get('/', 'StaticPagesController@index');
+Route::get('home', 'StaticPagesController@index');
