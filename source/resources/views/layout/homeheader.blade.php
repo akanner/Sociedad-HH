dc3fbd9f3ce3f56f6fc2863c4067c5b55d92f09c
leito9008 <leito.vm3@hotmail.com>
2015-06-29 00:28:06 -0300

    Logo del header. Pasar las clases a un .less + Elixir

--- source/resources/views/layout/homeheader.blade.php
+++ source/resources/views/layout/homeheader.blade.php
@@ -1,3 +1,14 @@
 <header class="home-header">
+    <div style="height:440px;">
+        <div style="  width: 940px;
+  background-color: lightcoral;
+  margin: 0 auto;height:100%;">
+            <img style="  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin-top: -110px;
+  margin-left: -220px;" src="{{asset('img/logo/logo-home.png')}}" alt="Logo sociedad hh">
+        </div>
+    </div>
 
 </header>
