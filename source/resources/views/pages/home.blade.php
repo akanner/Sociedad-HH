dcbf33983603146b48f1bca828ec485a601d3268
leito9008 <leito.vm3@hotmail.com>
2015-07-05 23:47:45 -0300

    Acomodado el width para 100%. Acomodado el footer, al fondo

--- source/resources/views/pages/home.blade.php
+++ source/resources/views/pages/home.blade.php
@@ -11,14 +11,14 @@
                 </div>
                 <div class="header-and-paragraph">
                     <h3>VISI&Oacute;N</h3>
-                    <p>Busca ser la entidad hematológica de referencia local, procurando ofrecer un lugar de encuentro para los colegas donde se atienda a sus demandas e intereses,  y se unan los esfuerzos de la especialidad, en pos de llevar adelante la excelencia en el ejercicio profesional para con la comunidad. </p>
+                    <p>Busca ser la entidad hematológica de referencia local, procurando ofrecer un lugar de encuentro para los colegas donde se atienda a sus demandas e intereses, y se unan los esfuerzos de la especialidad, en pos de llevar adelante la excelencia en el ejercicio profesional para con la comunidad. </p>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="header-and-paragraph">
                     <h3>OBJETIVO</h3>
                     <p>Su  objetivo es  obrar por la defensa, el respeto y el desarrollo de la especialidad,  intentando junto con ello, mejorar la calidad de la atención hematológica en la comunidad platense y zonas aledañas.
-        Por tal motivo, la entidad trabaja desde la actividad científica, al fomentar la investigación sobre temas inherentes a las dos especialidades. Mediante  la participación y comunicación entre sus miembros, pretende conseguir un progreso en la etiopatogenia, prevención, diagnóstico y tratamiento de las Enfermedades Hemáticas; incentivando además,  la formación académica, y la práctica médica.
+        Por tal motivo, la entidad trabaja desde la actividad científica, al fomentar la investigación sobre temas inherentes a las dos especialidades. Mediante la participación y comunicación entre sus miembros, pretende conseguir un progreso en la etiopatogenia, prevención, diagnóstico y tratamiento de las Enfermedades Hemáticas; incentivando además, la formación académica, y la práctica médica.
         Asimismo, apunta a lo comunitario, propiciando respuestas y calidad en la atención para el bienestar de la población.
         Y finalmente, promueve la lucha gremial por la mejora de las condiciones laborales y los honorarios de los hematólogos platenses.
         </p>
