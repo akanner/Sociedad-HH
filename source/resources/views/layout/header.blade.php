9c684af46b219828702ee77ab016ce4ed696a954
leito9008 <leito.vm3@hotmail.com>
2015-07-09 20:32:33 -0300

    Header chico

---
+++ source/resources/views/layout/header.blade.php
@@ -0,0 +1,11 @@
+<header class="default-header">
+    <div class="content-wrapper">
+        <div class="gradient-background">
+            <div class="left-gradient-background"></div>
+            <div class="logo-container">
+                <img src="{{asset('img/logo/logo-header.png')}}" alt="Logo sociedad hematologos y hemoterapeutas">
+            </div>
+            <div class="right-gradient-background"></div>
+        </div>
+    </div>
+</header>
