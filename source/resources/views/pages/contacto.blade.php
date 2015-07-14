2475f5899a3a0ae5128f5d95e5acf987675c97d7
leito9008 <leito.vm3@hotmail.com>
2015-07-13 23:53:41 -0300

    Terminada pagina de contacto

--- source/resources/views/pages/contacto.blade.php
+++ source/resources/views/pages/contacto.blade.php
@@ -11,7 +11,8 @@
         </div>
 
         <div class="contacts-wrapper">
-            <div class="contact">
+
+               <div class="contact">
                 <div class="contact-image">
                     <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
                 </div>
@@ -20,6 +21,37 @@
                     <p>Sh uvib qifwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia  fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia cyuvswa iyvcsia cyvsai fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia dhwsvai yvcisa ibxiusw viudws iuwasi cyvwius iuswa. </p>
                 </div>
             </div>
+
+            <div class="contact">
+                <div class="contact-image">
+                    <img src="{{asset('img/contacto/hombre.png')}}" alt="mari" class="img-rounded">
+                </div>
+                <div class="contact-description header-double-dash-and-paragraph">
+                    <h3>Fulana de tal. <span>Presidente</span></h3>
+                    <p>Sh uvib qifwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia  fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia cyuvswa iyvcsia cyvsai fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia dhwsvai yvcisa ibxiusw viudws iuwasi cyvwius iuswa. </p>
+                </div>
+            </div>
+
+            <div class="contact">
+                <div class="contact-image">
+                    <img src="{{asset('img/contacto/mujer.png')}}" alt="mari" class="img-rounded">
+                </div>
+                <div class="contact-description header-double-dash-and-paragraph">
+                    <h3>Fulana de tal. <span>Presidente</span></h3>
+                    <p>Sh uvib qifwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia  fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia cyuvswa iyvcsia cyvsai fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia dhwsvai yvcisa ibxiusw viudws iuwasi cyvwius iuswa. </p>
+                </div>
+            </div>
+
+            <div class="contact">
+                <div class="contact-image">
+                    <img src="{{asset('img/contacto/nati.png')}}" alt="mari" class="img-rounded">
+                </div>
+                <div class="contact-description header-double-dash-and-paragraph">
+                    <h3>Fulana de tal. <span>Presidente</span></h3>
+                    <p>Sh uvib qifwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia  fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia cyuvswa iyvcsia cyvsai fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia dhwsvai yvcisa ibxiusw viudws iuwasi cyvwius iuswa. </p>
+                </div>
+            </div>
+
         </div>
 
     </div>
