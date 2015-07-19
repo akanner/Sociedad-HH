a621b2034712906ea9eda2e5feeb75c64fc833fe
Agustin Ignacio Kanner <agustinkanner@gmail.com>
2015-07-19 03:02:30 -0300

    Le agregue una clase al div que contiene las imagenes de cada pregunta Subi una imagen de prueba.

--- source/resources/views/pages/surveys/templates/question.blade.php
+++ source/resources/views/pages/surveys/templates/question.blade.php
@@ -1,2 +1,10 @@
 <h4>{{$questionNumber}}.{{$question->getDescription()}}</h4>
+<p>
+    @foreach ($question->getPictures() as $picture)
+        <div class="question-img">
+            <img src="{{asset($picture->getPath())}}" alt="" />
+        </div>
+
+    @endforeach
+</p>
 @include($question->getTemplateName(),['question'=>$question])
