be8d7c198de4fbfa4fabc572bdd82b6e58fd65f8
Agustin Ignacio Kanner <agustinkanner@gmail.com>
2015-07-21 23:23:04 -0300

    Ahora se muestran los numeros de las preguntas correctamente

--- source/resources/views/pages/questionnaires/completeQuestionnaire.blade.php
+++ source/resources/views/pages/questionnaires/completeQuestionnaire.blade.php
@@ -4,12 +4,13 @@
 @section('content')
   <h2>{{$questionnaire->getTitle()}}</h2>
   <form method="post">
+      <input type="hidden" value="{{$questionnaire->id}}"/>
     <div>
-      @foreach ($questionnaire->getQuestions() as $question)
-        <div class="questionnaire-question">
-          @include('pages.questionnaires.templates.question',['question'=>$question,'questionNumber'=>1])
-        </div>
-      @endforeach
+        @for ($i = 0; $i < $questionnaire->getQuestions()->count(); $i++)
+            <div class="questionnaire-question">
+                @include('pages.questionnaires.templates.question',['question'=>$questionnaire->getQuestions()->get($i),'questionNumber'=>$i+1])
+            </div>
+        @endfor
     </div>
     <button type="submit" class="btn btn-error">Enviar</button>
   </form>
