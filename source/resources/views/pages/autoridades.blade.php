190a9f1bda95f66e9f3084c9f70c585fdd3b1fc2
leito9008 <leito.vm3@hotmail.com>
2015-08-12 00:53:27 -0300

    Cambiada la seccion de autoridades siguiendo el nuevo diseño

--- source/resources/views/pages/autoridades.blade.php
+++ source/resources/views/pages/autoridades.blade.php
@@ -12,43 +12,123 @@
 
         <div class="contacts-wrapper">
 
-               <div class="contact">
-                <div class="contact-image">
-                    <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+            <div class="col-md-6">
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. Graciela Klein. <span>Presidente</span></h3>
+                    </div>
                 </div>
-                <div class="contact-description header-double-dash-and-paragraph">
-                    <h3>Fulana de tal. <span>Presidente</span></h3>
-                    <p>Sh uvib qifwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia  fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia cyuvswa iyvcsia cyvsai fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia dhwsvai yvcisa ibxiusw viudws iuwasi cyvwius iuswa. </p>
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. Rosana Taus. <span>Secretaria</span></h3>
+                    </div>
+                </div>
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dr. Gustavo Marín. <span>Secretario Científico</span></h3>
+                    </div>
+                </div>
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. Sandra Rubbo. <span>Vocal</span></h3>
+                    </div>
+                </div>
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. Julieta Dalmaroni. <span>Vocal</span></h3>
+                    </div>
+                </div>
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. María Victoria Canessa. <span>Vocal</span></h3>
+                    </div>
+                </div>
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. Susana Bunzel. <span>Representante para
+la evaluación de las Recertificaciones</span></h3>
+                    </div>
                 </div>
             </div>
 
-            <div class="contact">
-                <div class="contact-image">
-                    <img src="{{asset('img/contacto/hombre.png')}}" alt="mari" class="img-rounded">
+            <div class="col-md-6">
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. María Silvia Matanó. <span>Vicepresidente</span></h3>
+                    </div>
                 </div>
-                <div class="contact-description header-double-dash-and-paragraph">
-                    <h3>Fulana de tal. <span>Presidente</span></h3>
-                    <p>Sh uvib qifwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia  fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia cyuvswa iyvcsia cyvsai fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia dhwsvai yvcisa ibxiusw viudws iuwasi cyvwius iuswa. </p>
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. Susana Bunzel. <span>Tesorera</span></h3>
+                    </div>
                 </div>
-            </div>
-
-            <div class="contact">
-                <div class="contact-image">
-                    <img src="{{asset('img/contacto/mujer.png')}}" alt="mari" class="img-rounded">
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. Soledad Crucet. <span>Revisor de Cuentas</span></h3>
+                    </div>
                 </div>
-                <div class="contact-description header-double-dash-and-paragraph">
-                    <h3>Fulana de tal. <span>Presidente</span></h3>
-                    <p>Sh uvib qifwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia  fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia cyuvswa iyvcsia cyvsai fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia dhwsvai yvcisa ibxiusw viudws iuwasi cyvwius iuswa. </p>
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. Liliana Alba. <span>Vocal</span></h3>
+                    </div>
                 </div>
-            </div>
-
-            <div class="contact">
-                <div class="contact-image">
-                    <img src="{{asset('img/contacto/nati.png')}}" alt="mari" class="img-rounded">
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. Diana Sccebba. <span>Vocal</span></h3>
+                    </div>
                 </div>
-                <div class="contact-description header-double-dash-and-paragraph">
-                    <h3>Fulana de tal. <span>Presidente</span></h3>
-                    <p>Sh uvib qifwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia  fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia cyuvswa iyvcsia cyvsai fwu biqwe qdwb vusa vcuwv csu csu si uwad iusvc usadvi jcwsyvc chsviu csvia dhwsvai yvcisa ibxiusw viudws iuwasi cyvwius iuswa. </p>
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. Griselda Barrientos. <span>Vocal</span></h3>
+                    </div>
+                </div>
+                <div class="contact">
+                    <div class="contact-image">
+                        <img src="{{asset('img/contacto/mari.png')}}" alt="mari" class="img-rounded">
+                    </div>
+                    <div class="contact-description header-double-dash-and-paragraph">
+                        <h3>Dra. Marta Gelemur. <span>Representante para
+la evaluación de las Recertificaciones</span></h3>
+                    </div>
                 </div>
             </div>
 
--- source/resources/views/pages/questionnaires/completeQuestionnaire.blade.php
+++ source/resources/views/pages/questionnaires/completeQuestionnaire.blade.php
@@ -2,25 +2,38 @@
 @section('title') Completar Encuesta :: @parent @stop
 
 @section('content')
-    <h2>{{$questionnaire->getTitle()}}</h2>
-    {!! Form::open(array('action' => 'QuestionnaireController@completeQuestionnaire')) !!}
-        {!! Form::hidden("questionnaireId",$questionnaire->id) !!}
-        <div class="user-email">
-            {!! Form::label('email', 'Dirección de Email') !!}
-            {!! Form::email('email_address',null,["required"]) !!}
+
+    <div class="row">
+        @include('errors.messageBagErrors')
+
+        <div class="page-intro-wrapper">
+            <h4>CONSTRUYAMOS NUESTRO CONOCIMIENTO</h4>
+            <h3>{{$questionnaire->getTitle()}}</h3>
+            <p>{{$questionnaire->getDescription()}}</p>
         </div>
-        <div class="user-name">
-            {!! Form::label('name', 'Nombre y apellido') !!}
-            {!! Form::text('userName') !!}
-        </div>
-        <div>
-            @for ($i = 0; $i < $questionnaire->getQuestions()->count(); $i++)
-                <div class="questionnaire-question">
-                    @include('pages.questionnaires.templates.question',['question'=>$questionnaire->getQuestions()->get($i),'questionNumber'=>$i+1])
+
+        {!! Form::open(array('action' => 'QuestionnaireController@completeQuestionnaire', 'class' => 'stylish-form')) !!}
+            {!! Form::hidden("questionnaireId",$questionnaire->id) !!}
+            <div class="user-info">
+                <p>Por favor déjenos sus datos para poder luego enviarle la clase virtual</p>
+                <div class="form-group">
+                    {!! Form::label('name', 'Nombre y apellido') !!}
+                    {!! Form::text('userName', null, ["class" => "form-control"]) !!}
                 </div>
-            @endfor
-        </div>
-        <button type="submit" class="btn btn-error">Enviar</button>
-    {!! Form::close() !!}
+                <div class="form-group">
+                    {!! Form::label('email', 'Email') !!}
+                    {!! Form::email('email_address', null, ["class" => "form-control", "required"]) !!}
+                </div>
+            </div>
+            <div>
+                @for ($i = 0; $i < $questionnaire->getQuestions()->count(); $i++)
+                    <div class="questionnaire-question">
+                        @include('pages.questionnaires.templates.question',['question'=>$questionnaire->getQuestions()->get($i),'questionNumber'=>$i+1])
+                    </div>
+                @endfor
+            </div>
+            <button id="sendQuestionnaireButton" type="submit" class="btn btn-error">Enviar</button>
+        {!! Form::close() !!}
+    </div>
 
 @endsection
