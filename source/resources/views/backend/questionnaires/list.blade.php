996cfd0699ae60721d511d7deea906b17af84bbb
leito9008 <leito.vm3@hotmail.com>
2015-09-30 23:03:38 -0300

    Botón de enviar invitación

--- source/resources/views/backend/questionnaires/list.blade.php
+++ source/resources/views/backend/questionnaires/list.blade.php
@@ -3,6 +3,7 @@
 
 @section("content")
 <div class="row">
+
     <div class="header-and-paragraph">
         <h3>Listado de cuestionarios</h3>
     </div>
@@ -14,22 +15,23 @@
                 <tr>
                     <th>#</th>
                     <th>Nombre</th>
-                    <th>Activa Desde</th>
+                    <th>Activo Desde</th>
                     <th>Acciones</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($questionnaires as $key => $questionnaire)
-                    <tr class="questionnaire-row" data-status='{{$questionnaire->isActive()}}'>
-                        <td>{{$questionnaire->id}}</td>
-                        <td><a href="/encuestas/{{$questionnaire->id}}">{{$questionnaire->title}}</a></td>
-                        <td>{{$questionnaire->activeFrom}}</td>
-                        <td>
-                            <a class="btn btn-success btn-xs" data-id="{{$questionnaire->id}} href="/adminhh/encuestas/reporte/{{$questionnaire->id}}">Reporte</a>
-                            <button data-url="/adminhh/encuestas/flagQuestionnaireAs" class="btn btn-danger btn-xs delete-questionnaire" type="button">Finalizar</button>
-                            <button data-url="/adminhh/encuestas/flagQuestionnaireAs" class="btn btn-primary btn-xs activate-questionnaire" type="button" >Activar</button>
-                        </td>
-                    </tr>
+                <tr class="questionnaire-row" data-status='{{$questionnaire->isActive()}}'>
+                    <td>{{$questionnaire->id}}</td>
+                    <td><a href="/encuestas/{{$questionnaire->id}}">{{$questionnaire->title}}</a></td>
+                    <td>{{$questionnaire->activeFrom}}</td>
+                    <td>
+                        <a class="btn btn-success btn-xs" data-id="{{$questionnaire->id}}" href="/adminhh/encuestas/reporte/{{$questionnaire->id}}">Reporte</a>
+                        <button class="btn btn-primary btn-xs" data-id="{{$questionnaire->id}}">Invitar</button>
+                        <button data-url="/adminhh/encuestas/flagQuestionnaireAs" class="btn btn-danger btn-xs delete-questionnaire" type="button">Finalizar</button>
+                        <button data-url="/adminhh/encuestas/flagQuestionnaireAs" class="btn btn-primary btn-xs activate-questionnaire" type="button" >Activar</button>
+                    </td>
+                </tr>
                 @endforeach
             </tbody>
         </table>
