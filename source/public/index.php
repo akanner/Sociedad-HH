9d07091ecf61558d014c213e2399a460a85638a3
leito9008 <leito.vm3@hotmail.com>
2015-10-16 01:16:50 -0300

    Se actualizo el index.php para que setee el path de la carpeta public relativo a donde se encuentre este archivo

--- source/public/index.php
+++ source/public/index.php
@@ -35,6 +35,12 @@ require __DIR__.'/../bootstrap/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+// set the public path to this directory
+$app->bind('path.public', function() {
+    App\Utils\PathHelper::setPublicResourcesPath(__DIR__);
+    return __DIR__;
+});
+
 /*
 |--------------------------------------------------------------------------
 | Run The Application
