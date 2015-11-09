a823d151e8a29fb13bbacc3a8b79126efadd2b30
leito9008 <leian1306@gmail.com>
2015-11-08 23:06:58 -0300

    Se corrigieron un par de problemas en el template, faltan agregar los íconos

--- source/resources/views/emails/invitations.blade.php
+++ source/resources/views/emails/invitations.blade.php
@@ -13,7 +13,7 @@
     <div style="padding-bottom: 30px;">
         <div style="height: 210px; background-color: #c1272d">
             <div style="margin: 0 auto; width: 440px; padding-top: 20px;">
-                <img src="{{ elixir('img/logo/logo-header.png') }}" alt="Logo sociedad hematologos y hemoterapeutas">
+                <img src="{{asset('img/logo/logo-header.png')}}" alt="Logo sociedad hematologos y hemoterapeutas">
             </div>
         </div>
 
@@ -24,28 +24,28 @@
             <p>Estimadas y estimados colegas:</p>
             <p>La Sociedad de Hematología y Hermoterapia de La Plata los invita a participar de la siguiente encuesta:</p>
 
-            <a href="www.sociedadhh.com.ar/encuestas/{{ $questionnaireId }}" style="margin: 10px 0; display: inline-block; padding: 10px 30px; background-color: #c1272d; color: white; text-decoration: none; font-weight: bold; border-bottom: 5px solid #841E22;">CONTESTAR ENCUESTA</a>
+            <a href="http://www.sociedadhh.com.ar/encuestas/{{ $questionnaireId }}" style="margin: 10px 0; display: inline-block; padding: 10px 30px; background-color: #c1272d; color: white; text-decoration: none; font-weight: bold; border-bottom: 5px solid #841E22;">CONTESTAR ENCUESTA</a>
             <p>Al responderla, recibirán a través de sus mails una clase virtual sobre el tema abordado.</p>
             <p>Esperamos contar con sus participaciones. Desde ya muchas gracias.</p>
             <p style="padding-top: 15px; font-weight: bold;">Sociedad de Hematología y Hermoterapia de La Plata</p>
         </div>
 
-        <footer style="width: 800px; margin: 0 auto;">
+        <!--<footer style="width: 800px; margin: 0 auto;">
             <ul style="list-style: none; padding-left: 0;">
                 <li>
-                    <img src="{{ elixir('img/logo/email-phone.gif') }}" alt="Tel&eacute;fono de contacto" style="width: 30px; position: relative; top: 10px;">
+                    <img src="{{-- elixir('img/logo/email-phone.gif') --}}" alt="Tel&eacute;fono de contacto" style="width: 30px; position: relative; top: 10px;">
                     <span>0221-4225111</span>
                 </li>
                 <li>
-                    <img src="{{ elixir('img/logo/email-email.gif') }}" alt="Email de contacto" style="width: 30px; position: relative; top: 10px;">
+                    <img src="{{-- elixir('img/logo/email-email.gif') --}}" alt="Email de contacto" style="width: 30px; position: relative; top: 10px;">
                     <span>splatensehh@gmail.com</span>
                 </li>
                 <li>
-                    <img src="{{ elixir('img/logo/email-location.gif') }}" alt="Direcci&oacute; de contacto" style="width: 30px; position: relative; top: 10px;">
+                    <img src="{{-- elixir('img/logo/email-location.gif') --}}" alt="Direcci&oacute; de contacto" style="width: 30px; position: relative; top: 10px;">
                     <span>50 n° 374 e/ 2 y 3</span>
                 </li>
             </ul>
-        </footer>
+        </footer>-->
     </div>
 
 </body>
