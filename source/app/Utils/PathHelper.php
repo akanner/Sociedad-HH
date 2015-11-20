83729c8f41ff78307bfa5e141e8d70d019a3d214
leito9008 <leian1306@gmail.com>
2015-11-20 00:16:07 -0300

    Se acomodó la clase para que sea un singleton y use las funciones helpers de laravel ¬¬

--- source/app/Utils/PathHelper.php
+++ source/app/Utils/PathHelper.php
@@ -1,29 +1,39 @@
 <?php
 
 namespace App\Utils;
-/**
- *
- */
-class PathHelper
-{
-    private static $publicResourcesPath;
 
-    public static function getRootPath()
-    {
-        $rootPath  = dirname (dirname (dirname (__DIR__)));
-        return $rootPath;
+class PathHelper {
+
+    private static $instance;
+
+    /**
+     * Returns the *Singleton* instance of this class.
+     *
+     * @return Singleton The *Singleton* instance.
+     */
+    public static function getInstance() {
+        if (null === static::$instance) {
+            static::$instance = new static();
+        }
+        return static::$instance;
     }
 
-    public static function getPublicResourcesPath() {
-        return static::$publicResourcesPath;
+    /**
+     * Protected constructor to prevent creating a new instance of the
+     * *Singleton* via the `new` operator from outside of this class.
+     */
+    protected function __construct() {}
+
+    public function getRootPath() {
+        return base_path();
     }
 
-    public static function setPublicResourcesPath($newPublicResourcesPath) {
-        static::$publicResourcesPath = $newPublicResourcesPath;
+    public function getPublicResourcesPath() {
+        return public_path();
     }
 
-    public static function getPathToUploaded()
-    {
-        return static::getPublicResourcesPath() . "/uploaded";
+    public function getPathToUploaded() {
+        return $this->getPublicResourcesPath() . "/uploaded";
     }
+
 }
